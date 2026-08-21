7a1ec7b6e91a3b8798c71092099b0928b3b88879
mdaltoon10 <daltoonmamad4628@gmail.com>
2026-08-21 03:22:43 +0000

    fix: remove unused imports in web.go

--- internal/web/web.go
+++ internal/web/web.go
@@ -17,8 +17,6 @@ import (
 	"time"
 
 	"github.com/mdaltoon10/D-UI/v3/internal/config"
-	"github.com/mdaltoon10/D-UI/v3/internal/database"
-	"github.com/mdaltoon10/D-UI/v3/internal/database/model"
 	"github.com/mdaltoon10/D-UI/v3/internal/eventbus"
 	"github.com/mdaltoon10/D-UI/v3/internal/logger"
 	"github.com/mdaltoon10/D-UI/v3/internal/mtproto"
